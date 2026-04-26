7200fc55d5dc40661f6ef14b9e211931f9725b85
drishtirajput121-a11y <drishtirajput121@gmail.com>
2026-04-26 10:55:05 +0530

    homepage nearly done

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,13 @@
 
 from django.shortcuts import render
-
+from blogs.models import Category, Blog
 def home(request):
-    return render(request, 'home.html')
+    catogories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True, status="Published")
+    post=Blog.objects.filter(is_featured=False, status="Published")
+    context = {
+        'categories': catogories,
+        'featured_posts': featured_posts,
+        'posts': post,
+    }
+    return render(request, 'home.html', context)
